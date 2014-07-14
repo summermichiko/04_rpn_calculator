54163c0e6d030b3971492f11553b961a6125f64e
Summer <summermichiko@gmail.com>
2014-07-13 20:34:49 -0400

    passes all tests

--- rpnCalculator.js
+++ rpnCalculator.js
@@ -1,47 +1,54 @@
 function Calculator() {
-    var total = 0;
-    var stack = "";
+    this.stack = [];
+    this.error = "calculator is empty"
 
-    this.value = function(){
-    	return total;
-    };
-    this.push = function(number) {
-		stack = stack + number;
-	};
-	this.plus = function() {
-		var first = stack.pop();
-		var second = stack.pop();
-		var next = first + second;
-		stack.push(next);
-		//pop two elements from stack
-		//add them together
-		//push result back
-	};
-	this.minus = function() {
-		var first = stack.pop();
-		var second = stack.pop();
-		var next = first - second;
-		stack.push(next);
-		//pop two elements from stack
-		//subtract one from the other
-		//push result back
-	};
-	this.divide = function() {
-		var first = stack.pop();
-		var second = stack.pop();
-		var next = first/second;
-		stack.push(next);
-		//pop two elements from stack
-		//divide
-		//push result back
-	};
-	this.times = function() {
-		var first = stack.pop();
-		var second = stack.pop();
-		var next = first * second;
-		stack.push(next);
-		//pop two elements from stack
-		//multiply together
-		//push result back
-	};
-};
+
+	this.push = function(number) {
+			this.stack.push(number);
+	}
+	    
+	this.value = function() {
+	    return this.stack[this.stack.length-1]; //gives last item on the stack
+	}
+
+	this.plus = function(input) {
+	    if (this.stack.length < 2) {
+			throw this.error;
+		} else {
+			result = this.stack.pop() + this.stack.pop(); 
+			this.stack.push(result); 
+		}
+	}
+
+	this.minus = function(input) {
+	    if (this.stack.length < 2) {
+			throw this.error;
+		} else {
+			result = -this.stack.pop() + this.stack.pop(); 
+			this.stack.push(result); 
+		}
+	}
+
+	this.divide = function(input) {
+	    if (this.stack.length < 2) {
+			throw this.error;
+		} else {
+			var first = this.stack.pop();
+			var second = this.stack.pop();
+			result = second / first; 
+			this.stack.push(result); 
+		}
+	}
+
+	this.times = function(input) {
+	    if (this.stack.length < 2) {
+			throw this.error;
+		} else {
+			result = this.stack.pop() * this.stack.pop(); 
+			this.stack.push(result); 
+		}
+	}
+}
+
+
+
